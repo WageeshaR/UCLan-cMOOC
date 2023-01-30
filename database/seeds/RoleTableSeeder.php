60f01e307c3838c6cbbad38856b002ab19711958
Wageesha Rasanjana <wageesha.rcc07@gmail.com>
2023-01-30 11:27:21 +0000

    Correct RoleSeeder

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -12,16 +12,19 @@ class RoleTableSeeder extends Seeder
         $role_student = new Role();
         $role_student->name = 'student';
         $role_student->description = 'Student to learn course';
+        $role_student->is_active = true;
         $role_student->save();
 
         $role_instructor = new Role();
         $role_instructor->name = 'instructor';
         $role_instructor->description = 'Instructor to manage course';
+        $role_instructor->is_active = true;
         $role_instructor->save();
 
         $role_admin = new Role();
         $role_admin->name = 'admin';
         $role_admin->description = 'Admin to manage the site';
+        $role_admin->is_active = true;
         $role_admin->save();
     }
   }
